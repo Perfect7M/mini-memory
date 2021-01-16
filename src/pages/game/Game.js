be5dbafe7ae2ab494bb9ebd0af66575ce68a5303
Marcin Skrzyński <marcin.skrzynski@likims.com>
2021-01-16 12:55:10 +0100

    split application into pages
    * add react-router package
    * add desired pages with boilerplate code
    * move test to separate files
    * clean up redundant files

---
+++ src/pages/game/Game.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./Game.module.css";
+
+export default function Game() {
+  return <div className={styles.row}>Game</div>;
+}
---
+++ src/pages/leaderboards/Leaderboards.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./Leaderboards.module.css";
+
+export default function Leaderboards() {
+  return <div className={styles.row}>Leaderboards</div>;
+}
